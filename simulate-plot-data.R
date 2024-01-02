233b38d01589a9c112cd612d2d8f34eaa378902f
Alyssa <alysalazar20@gmail.com>
2024-01-02 12:41:01 -0700

    forgot to select what needed to be committed.

--- simulate-plot-data.R
+++ simulate-plot-data.R
@@ -7,3 +7,8 @@ x <- rnorm(n = 100)
 y <- 2 * x + rnorm(n = 100, sd = 0.2)
 
 plot(x = x, y = y)
+
+# i will have the best 2024 and travel, love my body, make it to korean language level 3 on ttmik, get my TEFL certificate
+# learn a kpop dance, save $1,000 by the end of Jan, get a new car, and stop procrastinating
+
+x <- 2024
